3f94c37472e291a554db4aef919e531eb6ca61c6
Matheus Osterno <matheusosternodev@gmail.com>
2022-11-17 06:42:01 -0300

    Ajuste2

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import bootstrap from './bootstrap'
-import VueCarousel from 'vue-carousel';
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
 
-createApp(App).use(store).use(router).use(bootstrap).mount('#app').use(VueCarousel);
+createApp(App).use(store).use(router).use(bootstrap).mount('#app');
 
 
